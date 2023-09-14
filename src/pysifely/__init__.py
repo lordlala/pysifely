e8623b981a15fda7c779cc169318367dbd0a22f4
lordlala <jasondanieladams@gmail.com>
2023-09-14 09:55:41 -0400

    remove copyright

--- src/pysifely/__init__.py
+++ src/pysifely/__init__.py
@@ -12,11 +12,6 @@ __author__ = 'Jason Adams'
 __license__ = 'MIT'
 #endregion begin
 
-#  Copyright (c) 2021. Mulliken, LLC - All Rights Reserved
-#  You may use, distribute and modify this code under the terms
-#  of the attached license. You should have received a copy of
-#  the license with this file. If not, please write to:
-#  joshua@mulliken.net to receive a copy
 import logging
 import time
 from typing import Dict, Any, List, Optional, Set
